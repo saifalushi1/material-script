eb90ce7e76ea1a260b0c1a0b7bc54c30919ada9a
Saif Alushi <alushisaif@gmail.com>
2022-09-21 00:48:52 -0500

    Update cliscript.py

--- cliscript.py
+++ cliscript.py
@@ -1,7 +1,7 @@
 #module require for running cli commands
 import subprocess
 
-#Starter Code (run this code and see what happends!)
+#Starter Code (run this code and see what happens!)
 subprocess.call("ls")
 
 #Build out a program similar to the javascript file
